82066a3e1252195131185023a1e68f0a542325c7
mhoffnn <matheushofmarck@gmail.com>
2023-06-15 16:25:24 -0300

    remove pass to search just one time by a point

--- avioane.js
+++ avioane.js
@@ -1,4 +1,4 @@
-let gridx = 16, gridy = 16;
+let gridX = 16, gridY = 16;
 let player = [], computer = [];
 let playerAirplanes = [], computerAirplanes = [];
 let playerLives = 0, computerLives = 0;
@@ -49,8 +49,10 @@ function preview() {
 }
 
 function startGame() {
-	if (!(player = initializePlayer(false))) return;
-	if (!(computer = initializeComputer())) return;
+	player = initializePlayer(false)
+	computer = initializeComputer()
+	if (player != []) return;
+	if (computer != []) return;
 
 	playerTurn = true;
 	document.getElementById("setupTable").style.display = "none";
@@ -65,9 +67,9 @@ function initializePlayer(preview) {
 	let x, y, j, p, varfx, varfy, nrAvion = 0, orientare;
 
 	let grid = [];
-	for (y = 0; y < gridx; ++y) {
+	for (y = 0; y < gridX; ++y) {
 		grid[y] = [];
-		for (x = 0; x < gridx; ++x)
+		for (x = 0; x < gridX; ++x)
 			grid[y][x] = ["e", -1, 0];
 	}
 
@@ -80,7 +82,7 @@ function initializePlayer(preview) {
 		if (varfx == "" || varfy == "") {
 			if (!preview) {
 				alert("Fill in the board with 5 planes!!!");
-				return false;
+				return [];
 			}
 			else
 				continue;
@@ -93,26 +95,26 @@ function initializePlayer(preview) {
 
 		// checking the position of the airplane 
 
-		if (varfx < 0 || varfx >= gridx || varfy < 0 || varfy >= gridy || grid[varfy][varfx][0] == "a") {
+		if (varfx < 0 || varfx >= gridX || varfy < 0 || varfy >= gridY || grid[varfy][varfx][0] == "a") {
 			alert("Plane " + j + " was not set right!!!");
 			document.getElementById("pozx" + j).focus();
-			return false;
+			return [];
 		}
 
 		for (p = 0; p < 9; p++) {
 			let pozx = positions[orientare][p][0] + varfx;
 			let pozy = positions[orientare][p][1] + varfy;
 
-			if (pozx >= gridx || pozx < 0 || pozy >= gridy || pozy < 0) {
+			if (pozx >= gridX || pozx < 0 || pozy >= gridY || pozy < 0) {
 				alert("Plane " + j + " was not set right!!!");
 				document.getElementById("pozx" + j).focus();
-				return false;
+				return [];
 			}
 
 			if (grid[pozy][pozx][0] == "a") {
 				alert("Plane " + j + " overlaps another plane!!!");
 				document.getElementById("pozx" + j).focus();
-				return false;
+				return [];
 			}
 		}
 
@@ -136,9 +138,9 @@ function initializePlayer(preview) {
 function initializeComputer() {
 	let y, x, p, grid, orientare;
 	grid = [];
-	for (y = 0; y < gridx; ++y) {
+	for (y = 0; y < gridX; ++y) {
 		grid[y] = [];
-		for (x = 0; x < gridx; ++x)
+		for (x = 0; x < gridX; ++x)
 			grid[y][x] = ["e", -1, 0];
 	}
 	let j, ok, varfx, varfy, nrAvion = 0;
@@ -151,17 +153,22 @@ function initializeComputer() {
 			ok = true;
 
 			// checking the position of the airplane
-			if (grid[varfy][varfx][0] == "a") { ok = false; continue; }
+			if (grid[varfy][varfx][0] == "a") {
+				ok = false;
+				continue;
+			}
 
 			for (p = 0; p < 9; p++) {
 				let pozx = positions[orientare][p][0] + varfx;
 				let pozy = positions[orientare][p][1] + varfy;
-				if ((pozx >= gridx || pozx < 0) || (pozy >= gridy || pozy < 0)) {
-					ok = false; break;
+				if ((pozx >= gridX || pozx < 0) || (pozy >= gridY || pozy < 0)) {
+					ok = false;
+					break;
 				}
 
 				if (grid[pozy][pozx][0] == "a") {
-					ok = false; break;
+					ok = false;
+					break;
 				}
 			}
 
@@ -199,22 +206,22 @@ function setImage(y, x, id, isComputer) {
 function displayGrid(isComputer) {
 	let y, x, s;
 	s = '<div class="pixel"></div>';
-	for (y = 0; y < gridy; y++) {
+	for (y = 0; y < gridY; y++) {
 		s += '<div class="pixel">' + (y + 1) + '</div>';
 	}
 	s += '<div style="clear:left;"></div>';
 	if (isComputer) {
-		for (y = 0; y < gridy; y++) {
+		for (y = 0; y < gridY; y++) {
 			s += '<div class="pixel">' + (y + 1) + '</div>';
-			for (x = 0; x < gridx; x++) {
+			for (x = 0; x < gridX; x++) {
 				s += '<a href="javascript:gridClick(' + y + ',' + x + ');"><img name="pc' + y + '_' + x + '" src="ave.gif" width=16 height=16></a>';
 			}
 			s += '<br>';
 		}
 	} else {
-		for (y = 0; y < gridy; y++) {
+		for (y = 0; y < gridY; y++) {
 			s += '<div class="pixel">' + (y + 1) + '</div>';
-			for (x = 0; x < gridx; x++) {
+			for (x = 0; x < gridX; x++) {
 				s += '<a href="javascript:playerGridClick(' + y + ',' + x + ');"><img name="ply' + y + '_' + x + '" src="av' + player[y][x][0] + '.gif" width=16 height=16></a>';
 			}
 			s += '<br>';
@@ -278,27 +285,26 @@ function gridClick(y, x) {
 }
 
 function computerGridClick() {
-	let x, y, pass;
-	let selectedX, selectedY;
+	let x, y, selectedX, selectedY;
 	let selected = false;
 
-	for (pass = 0; pass < 2; ++pass) {
-		for (y = 0; y < gridy && !selected; ++y) {
-			for (x = 0; x < gridx && !selected; ++x) {
-				if (player[y][x][0] == "t") {
-					selectedX = x; selectedY = y;
-					if (pass == 0) {
-						if ((x + 2 < gridx) && (player[y][x + 1][0] == "t") && ((player[y][x + 2][0] == "a") || (player[y][x + 2][0] == "e"))) { selectedX = x + 2; selected = true; }
-						else if ((x - 1 >= 0) && (x + 1 < gridx) && (player[y][x + 1][0] == "t") && ((player[y][x - 1][0] == "a") || (player[y][x - 1][0] == "e"))) { selectedX = x - 1; selected = true; }
-						else if ((y - 1 >= 0) && (y + 1 < gridy) && (player[y + 1][x][0] == "t") && ((player[y - 1][x][0] == "a") || (player[y - 1][x][0] == "e"))) { selectedY = y - 1; selected = true; }
-						else if ((y + 2 < gridy) && (player[y + 1][x][0] == "t") && ((player[y + 2][x][0] == "a") || (player[y + 2][x][0] == "e"))) { selectedY = y + 2; selected = true; }
-					}
-					else {
-						if ((x - 1 >= 0) && ((player[y][x - 1][0] == "a") || (player[y][x - 1][0] == "e"))) { selectedX = x - 1; selected = true; }
-						else if ((x + 1 < gridx) && ((player[y][x + 1][0] == "a") || (player[y][x + 1][0] == "e"))) { selectedX = x + 1; selected = true; }
-						else if ((y - 1 >= 0) && ((player[y - 1][x][0] == "a") || (player[y - 1][x][0] == "e"))) { selectedY = y - 1; selected = true; }
-						else if ((y + 1 < gridy) && ((player[y + 1][x][0] == "a") || (player[y + 1][x][0] == "e"))) { selectedY = y + 1; selected = true; }
-					}
+	for (y = 0; y < gridY && !selected; ++y) {
+		for (x = 0; x < gridX && !selected; ++x) {
+			if (player[y][x][0] == "t") {
+				selectedX = x; selectedY = y;
+
+				if ((x - 1 >= 0) && ((player[y][x - 1][0] == "a") || (player[y][x - 1][0] == "e"))) {
+					selectedX = x - 1;
+					selected = true;
+				} else if ((x + 1 < gridx) && ((player[y][x + 1][0] == "a") || (player[y][x + 1][0] == "e"))) {
+					selectedX = x + 1;
+					selected = true;
+				} else if ((y - 1 >= 0) && ((player[y - 1][x][0] == "a") || (player[y - 1][x][0] == "e"))) {
+					selectedY = y - 1;
+					selected = true;
+				} else if ((y + 1 < gridy) && ((player[y + 1][x][0] == "a") || (player[y + 1][x][0] == "e"))) {
+					selectedY = y + 1;
+					selected = true;
 				}
 			}
 		}
@@ -306,15 +312,15 @@ function computerGridClick() {
 
 	if (!selected) {
 		do {
-			selectedY = Math.floor(Math.random() * (gridy - 1));
-			selectedX = Math.floor(Math.random() * (gridx - 1));
+			selectedY = Math.floor(Math.random() * (gridY - 1));
+			selectedX = Math.floor(Math.random() * (gridX - 1));
 		} while ((player[selectedY][selectedX][0] == "t") || (player[selectedY][selectedX][0] == "d") || (player[selectedY][selectedX][0] == "m"));
 	}
 
 	if ((player[selectedY][selectedX][2] == "cap") || (player[selectedY][selectedX][0] == "a")) {
 		setImage(selectedY, selectedX, "t", false);
 		let nrAvion = player[selectedY][selectedX][1];
-		if(player[selectedY][selectedX][2] == "cap") {
+		if (player[selectedY][selectedX][2] == "cap") {
 			playerAirplanes[nrAvion] = 1;
 		}
 		if (--playerAirplanes[nrAvion] == 0) {
@@ -325,6 +331,7 @@ function computerGridClick() {
 	} else if (player[selectedY][selectedX][0] == "e") {
 		setImage(selectedY, selectedX, "m", false);
 	}
+
 	document.getElementById("playerLives").innerHTML = playerLives;
 }
 
@@ -342,8 +349,8 @@ function playerLostLive() {
 
 function shootDownAirplane(grid, nrAvion, isComputer) {
 	let y, x;
-	for (y = 0; y < gridx; ++y) {
-		for (x = 0; x < gridx; ++x) {
+	for (y = 0; y < gridX; ++y) {
+		for (x = 0; x < gridX; ++x) {
 			if (grid[y][x][1] == nrAvion)
 				if (isComputer) setImage(y, x, "d", true);
 				else setImage(y, x, "d", false);
@@ -355,8 +362,8 @@ function shootDownAirplane(grid, nrAvion, isComputer) {
 function displayEnemyAirplanes() {
 	console.log("DisplayAirplanes")
 	let y, x;
-	for (y = 0; y < gridx; ++y) {
-		for (x = 0; x < gridx; ++x) {
+	for (y = 0; y < gridX; ++y) {
+		for (x = 0; x < gridX; ++x) {
 			if (computer[y][x][0] == "t")
 				setImage(y, x, "d", true);
 			else if (computer[y][x][0] == "a")
